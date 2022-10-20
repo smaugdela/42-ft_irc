456c60d494942e31fbe0b556b9cee21be53c3a4f
ajearuth <ajearuth@student.42.fr>
2022-10-20 16:28:14 +0200

    in progress

---
+++ src/commands/motd.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   motd.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:24:21 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/10/20 14:25:44 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libs.hpp"
+
+// You get a listing of an IRC server’s standard “message of the day” with the /motd command
+
+// /motd
+
+// The information in this message is specified by the server administrator,
+//  and often lists their email address in case you need to contact them as a last resort 
+//  with some serious problem.
---
+++ src/commands/user.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   user.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:15:22 by ajearuth          #+#    #+#             */
+/*   Updated: 2022/10/20 14:39:08 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libs.hpp"
+
+//    Command: USER
+//    Parameters: <user> <mode> <unused> <realname>
+
+//    The USER command is used at the beginning of connection to specify
+//    the username, hostname and realname of a new user.
+
+//    The <mode> parameter should be a numeric, and can be used to
+//    automatically set user modes when registering with the server.  This
+//    parameter is a bitmask, with only 2 bits having any signification: if
+//    the bit 2 is set, the user mode 'w' will be set and if the bit 3 is
+//    set, the user mode 'i' will be set.  (See Section 3.1.5 "User
+//    Modes").
+
+//    The <realname> may contain space characters.
+
+//    Numeric Replies:
+
+//            ERR_NEEDMOREPARAMS              ERR_ALREADYREGISTRED
