f8ee6e5994c964f38a62266bd71d4b56dcad50a7
ajearuth <ajearuth@student.42.fr>
2022-10-25 18:17:48 +0200

    nick is done i think

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   nick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 12:27:06 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/10/25 12:04:19 by smagdela         ###   ########.fr       */
+/*   Updated: 2022/10/25 18:17:26 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@
 
 //    NICK command is used to give user a nickname or change the existing
 //    one.
+
+
+static bool special_char(std::string to_check)
+{
+	int i = 0;
+	while (to_check[i])
+	{
+		if (to_check[i] <= 'A' || to_check[i] >= 'Z' 
+		|| to_check[i] <= 'a' || to_check[i] >= 'z'
+		||  to_check[i] <= 0 || to_check[i] >= 9
+		|| to_check[i] != '-' || to_check[i] != '_' || to_check[i] != '['
+		|| to_check[i] != ']' || to_check[i] != '{' || to_check[i] != '}'
+		|| to_check[i] != '\\' ||to_check[i] != '`' || to_check[i] != '|')
+			return false;
+		i++;	
+	}
+	return true;
+}
 
 void nick(Server *serv, Message &msg)
 {
@@ -30,7 +48,12 @@ void nick(Server *serv, Message &msg)
 	else
 	{
 		std::string new_nick = msg.getParams()[0];
-		if (serv->getUser(new_nick) == NULL)
+		if (new_nick.length() > 9 || special_char(new_nick) == false)
+		{
+			str = ERR_NONICKNAMEGIVEN;
+			str += " :Invalid nickname parameter(s).";
+		}
+		if (serv->getUser(new_nick) == NULL && special_char(new_nick) == true)
 		{
 			msg.getSender()->setNickname(new_nick);
 			str = "NICK ";
