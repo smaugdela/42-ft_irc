b4e9869fd038df912102476c2d2f13fb5399ea17
Fahima BOUMELLIL <fboumell@e3r1p4.clusters.42paris.fr>
2022-10-27 12:35:58 +0200

    command motd done

--- src/commands/motd.cpp
+++ src/commands/motd.cpp
@@ -6,7 +6,7 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 14:24:21 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/10/26 18:20:27 by fboumell         ###   ########.fr       */
+/*   Updated: 2022/10/27 12:33:15 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@
 			RPL_MOTDSTART                   RPL_MOTD
 			RPL_ENDOFMOTD                   ERR_NOMOTD */
 
-
 void motd(Server *serv, Message &msg)
 {
-	std::string	str;
+	std::string					str;
+	std::vector<std::string>	ret;
 	
 
 	if (msg.getParams().size() > 1)
@@ -35,7 +35,38 @@ void motd(Server *serv, Message &msg)
 	}
 	else
 	{
-			
+		str = RPL_MOTDSTART;
+		str += " " + msg.getSender()->getNickname() + " :- ";
+		str += serv->getConfig()->getServerName() + " ";
+		str += "Message of the day - ";
+		msg.getSender()->send_to(str.c_str());
+		
+		std::ifstream	ifs("./src/config/motd.txt", std::ifstream::in);
+		if (ifs.good())
+		{
+			std::string	line;
+			while (getline(ifs, line))
+				ret.push_back(line);
+			for (std::vector<std::string>::iterator it = ret.begin(); it != ret.end(); it++)
+			{
+				str = RPL_MOTD;
+				str += " " + msg.getSender()->getNickname() + " ";
+				str += *it;
+				msg.getSender()->send_to(str.c_str());
+			}
+		}
+		else
+		{
+			str = ERR_NOMOTD;
+			str += " " + msg.getSender()->getNickname() + " ";
+			str += ":MOTD File is missing";
+			msg.getSender()->send_to(str.c_str());
+ 		}	
+		ifs.close();
+		
+		str = RPL_ENDOFMOTD;
+		str += " " + msg.getSender()->getNickname() + " ";
+		str += ":End of MOTD command";
 	}
 	msg.getSender()->send_to(str.c_str());
 }
